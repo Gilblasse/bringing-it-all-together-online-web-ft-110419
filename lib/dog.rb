186d98759700332f8472dd604165a74b294998a9
Nethelbert <gilblasse@gmail.com>
2019-12-12 00:41:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,7 @@ class Dog
       INSERT INTO dogs(name,breed) VALUES(?,?)
     SQL
     DB[:conn].execute(sql,self.name,self.breed)
-    @id = DB[:conn].execute("SELECT * FROM dogs").last.first
+    self.id = DB[:conn].execute("SELECT * FROM dogs").last.first
     self
   end
   
